42b0a8c7f8c0e09ade88e2e8c3ed14b2cc37d2ea
Rida Kodad <kodadreda@gmail.com>
2019-08-08 12:15:53 +0000

    Automatically backed up by Learn

--- spec/classes_and_instances_spec.rb
+++ spec/classes_and_instances_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Dog' do
   it 'is defined within lib/dog.rb' do
-    if file_includes_class_definition('../lib/dog.rb','Dog')
+    if file_includes_class_definition('lib/dog.rb','Dog')
       expect(defined?(Dog)).to be_truthy
       expect(Dog).to be_a(Class)
     end
